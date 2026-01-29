9cc4f7e55bd4d9afed109c1f6a426c8dbae5e231
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-01-29 09:44:27 +0700

    add

---
+++ src/main/java/com/enigmacamp/todonyadia/TodonyadiaApplication.java
@@ -0,0 +1,13 @@
+package com.enigmacamp.todonyadia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodonyadiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodonyadiaApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/enigmacamp/todonyadia/controller/Hello.java
@@ -0,0 +1,43 @@
+package com.enigmacamp.todonyadia.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+
+@RestController
+public class Hello {
+
+    // http:localhost:8080/hello
+//    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+//    public String getHello(){
+//    return "Hello yall";
+//    }
+
+    // spesific get
+    @GetMapping("/")
+    public String getAnotherHello(){ return "Hello with getMapping!"; }
+
+    // path variable
+    @GetMapping("/products/{id}")
+    public  String pathVar(@PathVariable String id){ return "Product id: " + id; }
+
+    // req params
+//    @GetMapping("/products")
+//    public String reqParams(@RequestParam(name = "page") String page){
+//        return "Get result params: " + page;
+//    }
+
+    // req params with 2 params and default value
+//    @GetMapping("/products")
+//    public String reqParamsWithLimit(
+//        @RequestParam(name = "page", defaultValue = "1") int page,
+//        @RequestParam(name = "size", defaultValue = "10") int size
+//    ){
+//        return "Page: " + page + ", size:" + size;
+//    }
+
+    @PostMapping("/products")
+    public String reqBody(@RequestBody HashMap<String, String> map){
+        return "Result: " + map;
+    }
+}
