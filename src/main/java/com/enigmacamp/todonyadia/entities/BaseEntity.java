b3766c375d36e9e9c4b7f058ad184db2d1f04b0a
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-01-31 20:32:00 +0700

    add: @JsonFormat for date and time

--- src/main/java/com/enigmacamp/todonyadia/entities/BaseEntity.java
+++ src/main/java/com/enigmacamp/todonyadia/entities/BaseEntity.java
@@ -1,14 +1,13 @@
 package com.enigmacamp.todonyadia.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.Getter;
-import org.hibernate.annotations.UuidGenerator;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
@@ -17,9 +16,11 @@ public class BaseEntity {
 
     @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdAt;
 
     @LastModifiedDate
     @Column(name = "modify_at")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime modifiedAt;
 }
